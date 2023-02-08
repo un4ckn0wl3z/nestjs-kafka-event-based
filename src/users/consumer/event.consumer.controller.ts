939f2764c6dc8abae00e63e23965b3e24e6e706a
un4kn0wl3z <admin@unknownclub.net>
2023-02-08 11:13:14 +0700

    fix: fix bugs customaxios

--- src/users/consumer/event.consumer.controller.ts
+++ src/users/consumer/event.consumer.controller.ts
@@ -4,21 +4,24 @@ import { LoggerHookGuard } from 'src/framework/guard/logger.hook.guard';
 import { CustomLoggerService } from 'src/framework/logger/logger.service';
 import { UserRegistedEvent } from '../dtos/consumer/user-registed.event';
 import { ConsumerEvents } from '../enums/consume.events.enum';
+import { PhotosApiService } from '../service/photo-api.service';
 import { UsersService } from '../service/users.service';
 
 @Controller()
 export class EventConsumerController {
-
   constructor(
     private usersService: UsersService,
+    private photoService: PhotosApiService,
     private logger: CustomLoggerService,
   ){
 
   }
   @EventPattern(ConsumerEvents.UserRegisted)
   @UseGuards(LoggerHookGuard({event: ConsumerEvents.UserRegisted}))
-  handleUserRegistered(data: UserRegistedEvent){
+  async handleUserRegistered(data: UserRegistedEvent){
     this.logger.info('[+] handleUserRegistered entered', data)
+    const res = await this.photoService.listPhoto()
+    this.logger.debug('[+] Test Axios with via photoservice', res)
     this.usersService.createUser(data)
   }
 }
